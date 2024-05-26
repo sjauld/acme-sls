09cf819ac0ee3672a96972ff5e769be538e876cb
Stuart Auld <stuart.auld@viostream.com>
2024-05-27 09:30:59 +1000

    Output the bucket #minor

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
+output "bucket" {
+  description = "The bucket, in case you want to serve some static content as well as using it for challenges"
+  value       = aws_s3_bucket.challenge
+}
+
 output "cname_records" {
   description = "You should create CNAME records as follows"
   value       = [for _, v in aws_s3_bucket.challenge : {name = v.bucket, record = v.bucket_domain_name}]
